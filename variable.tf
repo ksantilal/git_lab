c8997ce061e86c5838eecde27fa31e0176b80efd
kapil bamania <kapilbamania96@.com>
2026-07-19 20:50:50 +0100

    hotfix: update eks module and python playbook

---
+++ variable.tf
@@ -0,0 +1,57 @@
+1.main branch
+    1.Readme.md file
+ 
+2.release/july26
+ 
+3.dev/july26
+ 
+Create feature from dev/july26
+ 
+### Sprint 1: ###
+1.Develop terraform Module [#devops-123] feature/devops-123
+2.Develop Ansible Module [#devops-456] feature/devops-456
+ 
+Merge it to dev/july26
+ 
+Merge dev/july26 -> release/july26
+ 
+Merge release/july26 -> main branch
+ 
+Next:
+Cleanup or delete feature branches
+ 
+====================================
+ 
+Next Step:
+ 
+Create
+1.release/aug26
+ 
+2.dev/aug26
+ 
+Create feature from dev/aug26
+ 
+### Sprint2: ###
+1.In Terraform module, create eks module [#devops-789]
+2.In Ansible Module, create playbook for python [#devops987]
+ 
+Merge it to dev/aug26
+ 
+Merge dev/july26 -> release/aug26
+ 
+Merge release/july26 -> main branch
+ 
+Next:
+Cleanup or delete feature branches
+ 
+=================================================
+ 
+### Sprint3: ### Hotfixes ex. hotfix/devops-654
+ 
+Create hot fix branches from main
+ 
+1.In Terraform module, update eks module [#devops-654]
+2.In Ansible Module, update playbook for python [#devops313]
+ 
+Merge it to main branch
+Delete the hot fix branch
